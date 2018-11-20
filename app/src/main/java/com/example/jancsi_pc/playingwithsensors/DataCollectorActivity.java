b89ffac4d93731759802c0ec24c5e03b97e9929e
MilleJanos <millejanos31@gmail.com>
2018-11-20 15:44:14 +0200

    GitHub Fix - removed private file

--- app/src/main/java/com/example/jancsi_pc/playingwithsensors/DataCollectorActivity.java
+++ app/src/main/java/com/example/jancsi_pc/playingwithsensors/DataCollectorActivity.java
@@ -127,7 +127,7 @@ public class DataCollectorActivity extends AppCompatActivity implements SensorEv
         CheckWiFiNetwork();
 
         //Asking for connection
-        // RequireInternetConnection()
+        RequireInternetConnection();
 
         //FIREBASE INIT:
         mFirestore = FirebaseStorage.getInstance();
